822f8530563436e499fcbed01fa23cbecc2963af
rivaldo-bogar <androsbgr13@gmail.com>
2023-06-20 08:21:29 +0800

    Menambahkan file dan melanjutkan beberapa latihan

---
+++ method/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace training_method
+{           // disini adalah pembelajaran saya tentang function, first my function
+    class method
+    {
+        static void robot() // first function
+        {
+            Console.WriteLine("Robot berjalan ke kiri");
+
+        }
+        static void Main(String[] args) // Main function
+        {
+            Console.Clear();
+            robot();
+        }
+    }
+}
