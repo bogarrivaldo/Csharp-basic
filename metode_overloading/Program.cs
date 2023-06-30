1c2c3c4b65287defeddd77c596fdb35344d8ed34
rivaldo-bogar <androsbgr13@gmail.com>
2023-06-30 11:12:05 +0800

    feat : method overloading topic

--- metode_overloading/Program.cs
+++ metode_overloading/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// method_overloding adalah fungsi yang sama tapi parameter berbeda seperti kita lihat dibawah ini ada beberapa fungsi yang sama dengan
+// nilai atau type data dari para meternya berbeda beda.
+namespace mthd_ovrldng
+{
+    public class overloading
+    {
+        static double myfunction(int nil1, int nil2)
+        {
+
+            Console.WriteLine("Hello Myname : Valdo_GameDeveloper");
+            Console.WriteLine(nil1 + nil2);
+            return nil1 + nil2;
+        }
+
+        static double myfunction(double nil1, double nil2)
+        {
+
+            Console.WriteLine("Hello Myname : Valdo_GameDeveloper");
+            Console.WriteLine(nil1 + nil2);
+            return nil1 + nil2;
+        }
+        static void Main(String[] args)
+        {
+            Console.Clear();
+            myfunction(10, 10);
+            myfunction(15.55, 15.55);
+        }
+    }
+}
