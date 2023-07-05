cbf5bd2621960ad56bff7e9d3b9402a7cd75967c
rivaldo-bogar <androsbgr13@gmail.com>
2023-07-05 16:06:28 +0800

    class member topic

---
+++ class_member/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace memberclass
+{
+    class class_member
+    {
+        string myname = "Valdo_GameDeveloper"; // assigment operator
+        int age = 21; // assigment operator
+        string level = "Legend";
+        void assignvalue()
+        {
+            Console.WriteLine("Hello i'm : " + myname + "\n my age : " + age);
+        }
+
+
+        static void Main(String[] args)
+        {
+
+            class_member npc1 = new class_member();
+            class_member npc2 = new class_member();
+            npc1.level = "Mythic"; // Passing value
+            npc2.level = "Bronze";  // Passing value
+            npc1.assignvalue();
+            Console.WriteLine(npc1.level); // Show in the console
+            Console.WriteLine(npc2.level);  // Show in the console
+        }
+    }
+}
