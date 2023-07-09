a6058749c6c5d436f8939e5d9060cf61eb589fc5
rivaldo-bogar <androsbgr13@gmail.com>
2023-07-09 23:22:37 +0800

    feat : Inheritance topic

---
+++ Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class class1
+    {
+        public int speed;
+    }
+    class class2 : class1
+    {
+        public void killthat()
+        {
+            Console.WriteLine("Kill Enemy that");
+        }
+    }
+    class Start
+    {
+        static void Main(String[] args)
+        {
+            class2 kls2 = new class2();
+            Console.Clear();
+            Console.WriteLine(kls2.speed);
+            kls2.killthat();
+        }
+    }
+}
---
+++ properti/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace properti
+{
+    class traningProperti
+    {
+        string Myname;
+
+        void setgettraning()
+        {
+            get
+             {    // Belum mengerti
+                return Myname;
+            }
+            set
+            {
+                // Belum mengerti
+            }
+        }
+    }
+    class Start
+    {
+        static void Main(String[] args)
+        {
+
+        }
+    }
+}
