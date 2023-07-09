64e4c87d745e77b5dff3434b8d803470fbedc4c1
rivaldo-bogar <androsbgr13@gmail.com>
2023-07-09 21:58:43 +0800

    feat : constructor topic

---
+++ Constructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace constructor
+{   // Kegunaan dari constructor akan dipanggil diawal ketika program di jalankan
+    class learnconstruct
+    {
+        string myname, school;
+        int age;
+
+        learnconstruct(string paraname, string paraschool, int paraage) // Constructor
+        {
+            myname = paraname;
+            school = paraschool;
+            age = paraage;
+        }
+        void covervalue()
+        {
+            Console.WriteLine("My name : " + myname);
+            Console.WriteLine("My name : " + school);
+            Console.WriteLine("My name : " + age);
+        }
+        static void Main(String[] args)
+        {
+            learnconstruct myobj = new learnconstruct("BogarDev", "UniversitasKlabat", 21);
+            myobj.covervalue();
+            Console.WriteLine(myobj.myname); // just testing code
+
+        }
+    }
+}
