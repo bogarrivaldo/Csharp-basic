65c1f77e8403cf1075164a640aa5e367f91df9af
rivaldo-bogar <androsbgr13@gmail.com>
2023-07-23 17:13:57 +0800

    make new project firebase

---
+++ connectfirebaset_test/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Firebase.Database;  // Ada error pada bagian ini
+using System;
+
+namespace connectfirebasetest
+{
+    class Hello
+    {
+        void AdminSDK()
+        {
+            var pathToKey = "C:/Users/VALDO/Downloads/mydata.json";
+            FirebaseApp.Create(new AppOptions
+            {
+                Credential = GoogleCredential.FromFile(pathToKey),
+            });
+        }
+        void LayananFirebase()
+        {
+
+            var FirebaseURL = "https://csharpprogramtest-default-rtdb.firebaseio.com/";
+            var defaultInstance = FirebaseDatabase.DefaultInstance;
+            defaultInstance.Options.DataBaseUrl = new Url(FirebaseURL);
+
+            // Contoh membaca data dari Realtime Database
+            DatabaseReference reference = defaultInstance.GetReference("path/to/your/data");
+            var snapshot = await reference.GetValueAsync();
+
+        }
+    }
+    class Update : Hello
+    {
+        static void Main(String[] args)
+        {
+            Update test1 = new Update();
+            test1.AdminSDK();
+            test1.LayananFirebase();
+            Console.Clear();
+        }
+    }
+
+}
+
--- multiple_obj/Program.cs
+++ multiple_obj/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using bogarclass;
 namespace multiobj
 {
     class testing
@@ -11,7 +11,7 @@ namespace multiobj
             // valdo obj2 = new valdo();
             testing obj1 = new testing();
             Console.WriteLine(obj1.myname);
-            // Console.WriteLine(obj2.testes); untuk percobaan membaca variabel dari class berbeda belum jadi 
+            // Console.WriteLine(obj2.myname); //untuk percobaan membaca variabel dari class berbeda belum jadi 
             // dikarenakan bisa jadi vscode tidak realtime read directori, percobaan bisa dari unity atau microsoft visual studio
 
         }
